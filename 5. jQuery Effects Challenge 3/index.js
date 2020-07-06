da75151f0208ed6e0db0c8abd821e4587eaf983c
kencormican <ken.cormican@bt.com>
2020-07-06 15:46:14 +0000

    jQuery Effects Challenge 3 Added

---
+++ 5. jQuery Effects Challenge 3/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#button-card-1").click(function () {
+    $("#par-1").slideToggle(750);
+  });
+  $("#button-card-2").click(function () {
+    $("#par-2").slideToggle(1000);
+  });
+  $("#button-card-3").click(function () {
+    $("#par-3").slideToggle();
+  });
+});
